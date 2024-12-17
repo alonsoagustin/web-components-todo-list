2969beaea3bb11a7f84ab148fdd941ddeec944b7
Agustin Alonso <alonsoiagustin@gmail.com>
2024-12-17 12:46:19 +0100

    persist input checked status in localStorage

--- components/todo-list.js
+++ components/todo-list.js
@@ -30,20 +30,50 @@ class TodoList extends HTMLElement {
       currentInputs = JSON.parse(localStorage.getItem("inputs")) ?? [];
       localStorage.setItem(
         "inputs",
-        JSON.stringify([...currentInputs, { detail: event.detail }])
+        JSON.stringify([
+          ...currentInputs,
+          {
+            id: event.detail.id,
+            value: event.detail.value,
+            isCompleted: event.detail.isCompleted,
+            isDeleted: event.detail.isDeleted,
+          },
+        ])
       );
     });
 
-    for (const input of currentInputs) {
-      this.createActionItem(input.detail);
+    for (const currentInput of currentInputs) {
+      const actionItem = this.createActionItem(currentInput);
+      const span = actionItem.shadowRoot.querySelector("span");
+      const input = actionItem.shadowRoot.querySelector("input");
+
+      if (currentInput.isCompleted) {
+        span.classList.add("checked");
+        input.setAttribute("checked", "");
+      } else {
+        span.classList.remove("checked");
+      }
     }
   }
 
-  createActionItem(spanContent) {
+  createActionItem(detail) {
     const div = this.shadowRoot.querySelector("div");
     const actionItem = document.createElement("action-item");
+
     div.appendChild(actionItem);
-    actionItem.setAttribute("span-content", spanContent);
+    actionItem.setAttribute("span-content", detail.value);
+    actionItem.setAttribute("id", detail.id);
+
+    actionItem.addEventListener("status-input-checkbox", (event) => {
+      let currentInputs = JSON.parse(localStorage.getItem("inputs"));
+      currentInputs.map((input) => {
+        if (input.id === +event.detail.id) {
+          input.isCompleted = event.detail.status;
+          localStorage.setItem("inputs", JSON.stringify(currentInputs));
+        }
+      });
+    });
+    return actionItem;
   }
 }
 
